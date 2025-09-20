1b131f4284c7b20fe3563c7b2ef2909ffbc763ce
Phisher98 <mandiktest2@gmail.com>
2025-09-20 17:30:54 +0530

    Adding PortScanwithdomain.py

---
+++ PortScanner/PortScanwithdomain.py
@@ -0,0 +1,32 @@
+import socket
+from IPy import IP
+
+
+def check_ip(input):
+    try:
+        IP(input)
+        return input
+    except ValueError:
+        return socket.gethostbyname(input)
+
+def scan_port(ipaddress,port):
+    try:
+        sock= socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress,port))
+        print("Port " + str(port) + " is Open for " + str(ipaddress))
+    except:
+        print("Port " + str(port) + " is Closed for " + str(ipaddress))
+
+input = input("Enter an IP Address : ")
+actualip=check_ip(input)
+if(actualip.isdigit()):
+    Port_Range = input("Enter Port Range (Example 80-443): ")
+    SplitPorts = Port_Range.split("-")
+    starting_port = int(SplitPorts[0])
+    ending_port   = int(SplitPorts[1])
+
+    for port in range(starting_port,ending_port):
+        scan_port(actualip,port)
+else:
+    print("Error: Enter a valid IP Address")
